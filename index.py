a409b81b6367f908c701165042f20342e4de1a0d
Zack Stout <zackstout@gmail.com>
2018-03-23 20:10:37 -0500

    deleting sample code

--- index.py
+++ index.py
@@ -14,30 +14,6 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('fivethirtyeight')
 
-# right_table=soup.find('table', class_='wikitable sortable plainrowheaders')
-#
-# A=[]
-# B=[]
-# C=[]
-# D=[]
-# E=[]
-# F=[]
-# G=[]
-# for row in right_table.findAll("tr"):
-#     cells = row.findAll('td')
-#     states=row.findAll('th') #To store second column data
-#     if len(cells)==6: #Only extract table body not heading
-#         A.append(cells[0].find(text=True))
-#         B.append(states[0].find(text=True))
-#         C.append(cells[1].find(text=True))
-#         D.append(cells[2].find(text=True))
-#         E.append(cells[3].find(text=True))
-#         F.append(cells[4].find(text=True))
-#         G.append(cells[5].find(text=True))
-
-# print(C)
-
-
 
 # Shakespeare plays:
 # Holds urls:
